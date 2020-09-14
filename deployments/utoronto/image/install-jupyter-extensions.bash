527182367992de3ccf275c04f7806f39c6c9541b
YuviPanda <yuvipanda@gmail.com>
2020-09-14 20:15:46 +0530

    Add elfinder & jupyterlab proxy launcher icons
    Some folks will find this file management easier

--- deployments/utoronto/image/install-jupyter-extensions.bash
+++ deployments/utoronto/image/install-jupyter-extensions.bash
@@ -6,7 +6,8 @@ jupyter serverextension enable --sys-prefix jupyter_videochat
 jupyter labextension install --debug \
     @jupyter-widgets/jupyterlab-manager@2 \
     @jupyterlab/server-proxy@2.1.1 \
-    jupyterlab-videochat
+    jupyterlab-videochat \
+    @jupyterlab/server-proxy
 
 
 # Install jupyter-contrib-nbextensions
