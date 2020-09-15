a43a586986dc6790d0d82758dc3189da0b8ae09a
YuviPanda <yuvipanda@gmail.com>
2020-09-15 17:32:10 +0530

    Remove jupyter-videochat
    UToronto already has multiple supported video chat things,
    let's not muddle the water further.

--- deployments/utoronto/image/install-jupyter-extensions.bash
+++ deployments/utoronto/image/install-jupyter-extensions.bash
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Shouldn't be required, but alas.
-jupyter serverextension enable --sys-prefix jupyter_videochat
-
 jupyter labextension install --debug \
     @jupyter-widgets/jupyterlab-manager@2 \
     @jupyterlab/server-proxy@2.1.1 \
-    jupyterlab-videochat \
     @jupyterlab/server-proxy
 
 
